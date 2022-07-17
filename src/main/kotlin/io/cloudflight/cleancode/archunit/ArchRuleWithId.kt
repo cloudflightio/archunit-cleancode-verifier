ba2582b455e1134b2def1fd1a138989f7c40cdf3
Klaus Lehner <172195+klu2@users.noreply.github.com>
2022-07-17 12:40:11 +0200

    allow empty should for all rules (#5)

--- src/main/kotlin/io/cloudflight/cleancode/archunit/ArchRuleWithId.kt
+++ src/main/kotlin/io/cloudflight/cleancode/archunit/ArchRuleWithId.kt
@@ -1,11 +1,10 @@
 package io.cloudflight.cleancode.archunit
 
-import com.tngtech.archunit.ArchConfiguration
 import com.tngtech.archunit.lang.ArchRule
 import com.tngtech.archunit.library.freeze.FreezingArchRule.freeze
 
 internal class ArchRuleWithId(val id: String, rule: ArchRule) :
-    ArchRule by rule.because(idToUrl(id)) {
+    ArchRule by rule.because(idToUrl(id)).allowEmptyShould(true) {
 
     companion object {
         fun archRuleWithId(id: String, delegate: ArchRule): ArchRule {
