b98779ad321c4a4f968c5fa96670ab4d6996d12d
Klaus Lehner <172195+klu2@users.noreply.github.com>
2022-07-17 10:46:22 +0200

    only load tests if dependent classes are on the classpath

--- src/main/kotlin/io/cloudflight/cleancode/archunit/utils/ClassUtils.kt
+++ src/main/kotlin/io/cloudflight/cleancode/archunit/utils/ClassUtils.kt
@@ -1,9 +1,30 @@
 package io.cloudflight.cleancode.archunit.utils
 
+import com.tngtech.archunit.junit.ArchTests
+
 fun Class<*>.isKotlinClass(): Boolean {
     return this.declaredAnnotations.any {
         it.annotationClass == Metadata::class
     }
+}
+
+internal fun ruleSetOf(clazz: Class<*>, vararg requiredClasses: String): ArchTests {
+    return if (classesExistOnClasspath(*requiredClasses)) {
+        ArchTests.`in`(clazz)
+    } else {
+        ArchTests.`in`(Object::class.java)
+    }
+}
+
+internal fun classesExistOnClasspath(vararg classes: String): Boolean {
+    classes.forEach {
+        try {
+            Class.forName(it)
+        } catch (err: ClassNotFoundException) {
+            return false
+        }
+    }
+    return true
 }
 
 
