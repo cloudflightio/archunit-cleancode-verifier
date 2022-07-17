d92a3cc7220341d6b3642cd60f8ae03e4a2ff6a6
Klaus Lehner <172195+klu2@users.noreply.github.com>
2022-07-17 08:20:05 +0200

    update documentation for IdBasedViolationStore, cleanup freeze-logic

--- src/main/kotlin/io/cloudflight/cleancode/archunit/ArchRuleWithId.kt
+++ src/main/kotlin/io/cloudflight/cleancode/archunit/ArchRuleWithId.kt
@@ -2,7 +2,7 @@ package io.cloudflight.cleancode.archunit
 
 import com.tngtech.archunit.ArchConfiguration
 import com.tngtech.archunit.lang.ArchRule
-import com.tngtech.archunit.library.freeze.FreezingArchRule
+import com.tngtech.archunit.library.freeze.FreezingArchRule.freeze
 
 internal class ArchRuleWithId(val id: String, rule: ArchRule) :
     ArchRule by rule.because(idToUrl(id)) {
@@ -10,18 +10,19 @@ internal class ArchRuleWithId(val id: String, rule: ArchRule) :
     companion object {
         fun archRuleWithId(id: String, delegate: ArchRule): ArchRule {
             return if (FREEZE_ENABLED) {
-                FreezingArchRule.freeze(ArchRuleWithId(id, delegate)).persistIn(IdBasedViolationStore)
+                freeze(ArchRuleWithId(id, delegate)).persistIn(IdBasedViolationStore)
             } else {
                 ArchRuleWithId(id, delegate)
             }
         }
 
-        internal fun idToUrl(id:String):String {
+        internal fun idToUrl(id: String): String {
             return "$ROOT_URL/${id.substringBefore(".")}.md#user-content-${id.substringAfter(".")}"
         }
 
         private val VERSION = "v${ArchRuleWithId::class.java.`package`.implementationVersion}"
         private val ROOT_URL = "https://github.com/cloudflightio/archunit-cleancode-verifier/blob/$VERSION/rules"
-        private val FREEZE_ENABLED = true.toString() == ArchConfiguration.get().getPropertyOrDefault("cleancode.activate-freeze", false.toString())
+
+        internal var FREEZE_ENABLED = true
     }
 }
